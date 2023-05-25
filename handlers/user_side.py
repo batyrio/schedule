24770017d5d4cb829689efb9a451e59ba35f0ebc
batyrio <batyrio.wav@gmail.com>
2023-05-25 08:54:57 +0300

    a

---
+++ handlers/user_side.py
@@ -0,0 +1,19 @@
+from aiogram import types
+from create_bot import bot, dp
+from data_base import sqlite_db
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await bot.send_message(message.chat.id,
+                           'Приветствую! Бот - помощник для работников Лосиноостровской! Помощь по командам /help'
+                           )
+
+
+@dp.message_handler(commands=['news', 'новости'])
+async def news_command(message: types.Message):
+    news = await sqlite_db.get_news()
+    for i in news [:3]:
+        await bot.send_photo(message.chat.id, i[3], f'*{i[1]}*\n\n{i[2]}',
+                             parse_mode='Markdown'
+                             )
